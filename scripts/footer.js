2f462a164bf0677609fa2b5e05a80cff8b3757b6
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-12-07 04:54:47 +0200

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,5 +1,3 @@
-// Get the footer element by its ID
 const footerElement = document.getElementById('dynamicFooter');
-
-// Change the text content
-footerElement.innerHTML = '呪われた エンターテイメント <br> © Cursed Entertainment 2023';// JavaScript Document
+const currentYear = new Date().getFullYear();
+footerElement.innerHTML = `呪われた エンターテイメント <br> © Cursed Entertainment ${currentYear}`;
