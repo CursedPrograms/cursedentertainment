4499e7c8831917bfbb01fe5e99e1289602ef9b34
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-12-07 04:22:15 +0200

    Add files via upload

---
+++ scripts/footer.js
@@ -0,0 +1,5 @@
+// Get the footer element by its ID
+const footerElement = document.getElementById('dynamicFooter');
+
+// Change the text content
+footerElement.innerHTML = '呪われた エンターテイメント <br> © Cursed Entertainment 2023';// JavaScript Document
