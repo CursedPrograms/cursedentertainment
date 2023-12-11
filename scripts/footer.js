9c58563b137d43ab6ddca26d470f9109daf9f110
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-12-12 01:40:26 +0200

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,3 +1,4 @@
 const footerElement = document.getElementById('dynamicFooter');
-footerElement.innerHTML = '<a href="https://cursed-entertainment.itch.io/">呪われた エンターテイメント</a> <br> © Cursed Entertainment 2023';
+footerElement.innerHTML = '<a href="https://cursed-entertainment.itch.io/"><i>呪われた エンターテイメント</i></a>
+<br><a href="https://cursed-entertainment.netlify.app/pages/home.html"><small>© Cursed Entertainment 2023</small></a>';
 
