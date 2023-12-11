4c289ba2b7e1f652ea25ec0ded5156b3244b3bf3
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-12-12 01:44:42 +0200

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,2 +1,26 @@
+// Get the footer element by its ID
 const footerElement = document.getElementById('dynamicFooter');
-footerElement.innerHTML = '<a href="https://cursed-entertainment.itch.io/">呪われた エンターテイメント</a> <br> © Cursed Entertainment 2023';
+
+// Create a link element
+const linkElement = document.createElement('a');
+linkElement.href = 'https://cursed-entertainment.itch.io/';
+linkElement.textContent = '呪われた エンターテイメント';
+
+// Apply italic style to the link text
+linkElement.style.fontStyle = 'italic';
+
+// Create a line break element
+const lineBreakElement = document.createElement('br');
+
+// Create a copyright notice
+const copyrightNotice = document.createTextNode('© Cursed Entertainment 2023');
+
+// Apply italic style to the copyright notice
+copyrightNotice.parentElement.style.fontStyle = 'italic';
+// Apply small size to the copyright notice
+copyrightNotice.parentElement.style.fontSize = 'small';
+
+// Append the elements to the footer
+footerElement.appendChild(linkElement);
+footerElement.appendChild(lineBreakElement);
+footerElement.appendChild(copyrightNotice);
