17a0774424367f6ff92342e49e2af8fccde9a966
Cursed Entertainment <cursedsoft@gmail.com>
2023-12-12 22:22:16 +0200

    .js updates & headers

---
+++ scripts/get-holiday.js
@@ -0,0 +1,29 @@
+var currentDate = new Date();
+var currentMonth = currentDate.getMonth();
+var currentDay = currentDate.getDate();
+
+function setHolidayHeader() {
+    var header = document.getElementById('holidayHeader');
+    var logo;
+    if (currentMonth === 11 && (currentDay >= 24 && currentDay <= 25)) {
+        logo = '../images/christmasLogo.png';
+    } else if (currentMonth === 3 && (currentDay >= 1 && currentDay <= 7)) {
+        logo = '../images/easterLogo.png';
+    } else if (currentMonth === 9 && (currentDay >= 28 && currentDay <= 31)) {
+        logo = '../images/halloweenLogo.png';
+    }
+    if (!logo || !imageExists(logo)) {
+        logo = '../images/spr_cursedLogo.png';
+    }
+
+    header.innerHTML = '<img src="' + logo + '" alt="CursedEntertainment Logo" width="277" height="149">';
+}
+function imageExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status !== 404;
+}
+document.addEventListener('DOMContentLoaded', function() {
+    setHolidayHeader();
+});
---
+++ scripts/init-owl-carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  })
+});
