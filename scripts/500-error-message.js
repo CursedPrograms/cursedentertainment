d7a7f55c1cc6d87deef974589d73150d2a5d40f7
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2023-12-13 13:17:51 +0200

    Delete scripts directory

--- scripts/500-error-message.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var errorMessageContainer = document.getElementById("errorMessageContainer");
-    var errorMessageParagraph = document.createElement("p");
-    errorMessageParagraph.textContent = "Sorry, something went wrong on our end. We're working to fix it as soon as possible.";
-    errorMessageContainer.appendChild(errorMessageParagraph);
-});
