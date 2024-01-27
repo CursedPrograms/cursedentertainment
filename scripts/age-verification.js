88c8ebc104270768cc82a29719917da1463b2658
Cursed Entertainment <cursedsoft@gmail.com>
2024-01-28 01:48:42 +0200

    ...

--- scripts/age-verification.js
+++ scripts/age-verification.js
@@ -1,13 +1,20 @@
-function unblurImages() {
-    var age = document.getElementById("age").value;
-    if (age < 18) {
-      alert("You must be 18 or older to proceed.");
-      return false;
+document.addEventListener("DOMContentLoaded", function () {
+    function unblurImages() {
+      var age = document.getElementById("age").value;
+      if (age < 18) {
+        alert("You must be 18 or older to proceed.");
+        return false;
+      }
+      var images = document.getElementsByClassName("img");
+      for (var i = 0; i < images.length; i++) {
+        images[i].classList.add("unblur");
+      }
+      document.querySelector(".age-verification").style.display = "none";
+      return true;
     }
-    var images = document.getElementsByClassName("img");
-    for (var i = 0; i < images.length; i++) {
-      images[i].classList.add("unblur");
+    
+    var proceedButton = document.querySelector("button");
+    if (proceedButton) {
+      proceedButton.addEventListener("click", unblurImages);
     }
-    document.getElementsByClassName("age-verification")[0].style.display = "none";
-    return true;
-  }
+  });
