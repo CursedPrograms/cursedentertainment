1bae6e32cdb72c805475ed054cbdfe4957384675
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2024-10-20 12:46:54 +0200

    Add files via upload

---
+++ cursed-theme.js
@@ -0,0 +1,11 @@
+export const themes = {
+  default: {
+    title_color: "2f80ed",
+    icon_color: "4c71f2",
+    text_color: "434d58",
+    bg_color: "fffefe",
+    border_color: "e4e2e2",
+  }
+};
+
+export default themes;
